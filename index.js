968848b4729d3898879e67d55de8ed3b21e38afa
Ludvig-D <74721111+Ludvig-D@users.noreply.github.com>
2025-07-30 18:53:03 +0200

    Made list options work

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.argv[2] == 'add') {
       let newtask = {
         item: `${process.argv[3]}`,
         id: tasks.length + 1,
-        status: 'todo',
+        status: process.argv[4] || 'todo',
         createAt: new Date().getTime(),
         updatedAt: new Date().getTime(),
       };
@@ -54,14 +54,76 @@ if (process.argv[2] == 'add') {
   fs.readFile(jsonFilePath, (err, data) => {
     if (err) throw err;
     if (data.buffer.byteLength > 0) {
-      let tasks = JSON.parse(data);
-      tasks.map((task) => {
-        console.log(
-          `(ID:${task.id}) ${
-            task.item.charAt(0).toUpperCase() + task.item.slice(1)
-          }`
-        );
-      });
+      if (process.argv[3] == undefined) {
+        console.log('List of all tasks');
+        let tasks = JSON.parse(data);
+        tasks.map((task) => {
+          console.log(
+            `(ID:${task.id}) ${
+              task.item.charAt(0).toUpperCase() + task.item.slice(1)
+            } - ${task.status.charAt(0).toUpperCase() + task.status.slice(1)}`
+          );
+        });
+      } else if (process.argv[3] == 'done') {
+        let tasks = JSON.parse(data);
+        let listNum = tasks.filter((task) => task.status == 'done').length;
+        if (listNum > 0) {
+          console.log('List of all done tasks');
+          tasks.map((task) => {
+            if (task.status == 'done') {
+              console.log(
+                `(ID:${task.id}) ${
+                  task.item.charAt(0).toUpperCase() + task.item.slice(1)
+                } - ${
+                  task.status.charAt(0).toUpperCase() + task.status.slice(1)
+                }`
+              );
+            }
+          });
+        } else {
+          console.log('No tasks with status done');
+        }
+      } else if (process.argv[3] == 'todo') {
+        let tasks = JSON.parse(data);
+        let listNum = tasks.filter((task) => task.status == 'todo').length;
+        if (listNum > 0) {
+          console.log('List of all todo tasks');
+          tasks.map((task) => {
+            if (task.status == 'todo') {
+              console.log(
+                `(ID:${task.id}) ${
+                  task.item.charAt(0).toUpperCase() + task.item.slice(1)
+                } - ${
+                  task.status.charAt(0).toUpperCase() + task.status.slice(1)
+                }`
+              );
+            }
+          });
+        } else {
+          console.log('No tasks with status todo');
+        }
+      } else if (process.argv[3] == 'in-progress') {
+        let tasks = JSON.parse(data);
+        let listNum = tasks.filter(
+          (task) => task.status == 'in-progress'
+        ).length;
+        if (listNum > 0) {
+          tasks.map((task) => {
+            if (task.status == 'in-progress') {
+              console.log('List of all in-progress tasks');
+              console.log(
+                `(ID:${task.id})asasd ${
+                  task.item.charAt(0).toUpperCase() + task.item.slice(1)
+                } - ${
+                  task.status.charAt(0).toUpperCase() + task.status.slice(1)
+                }`
+              );
+            }
+          });
+        } else {
+          console.log('No tasks with status in-progress');
+        }
+      }
     } else {
       console.log('No tasks exist yet');
     }
